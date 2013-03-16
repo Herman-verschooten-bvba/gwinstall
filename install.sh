e5c860c07bc7a2db42440d52ccad908e0e6f760f
Herman verschooten <Herman@verschooten.net>
2013-03-16 16:51:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # GratWiFi Installation script
 #
 # (c) 2010-2013 Herman verschooten
 #
+# Verify Git is installed:
+if [ ! $(which git) ]; then
+  echo "Git is not installed, doing it now."
+  sudo apt-get -y install git-core
+fi
+
 echo "Downloading gwserver"
 git clone https://github.com/Herman-verschooten-bvba/gwserver.git 
 
 cd gwserver
 
-./install.sh
+echo "Now run sudo install.sh to continue"
